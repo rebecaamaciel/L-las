1d1962ab18b605fab1a761566662ff8ea6bbacd2
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-10-21 09:25:24 -0300

    fgfg

--- pergunta.js
+++ pergunta.js
@@ -1,81 +1,101 @@
-criarCartao(
-    ‘Bíblia’,
-    ‘Quem usou um cajado para abrir o Mar Vermelho?’,
-    ‘Moisés’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Quem sobreviveu dentro de uma cova cheia de leões?’,
-    ‘Daniel’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Quem construiu uma arca para sobreviver ao dilúvio?’,
-    ‘Noé’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Qual foi o primeiro milagre que Jesus fez?’,
-    ‘transformar a água em vinho’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Quantos discípulos Jesus tinha?’,
-    ‘12’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Com quantos anos Jesus morreu?’,
-    ‘33’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Quantos livros tem a Bíblia?’,
-    ‘66’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Qual o nome do discípulo de Jesus que andou sobre as águas?’,
-    ‘Pedro’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘Qual discípulo traiu Jesus?’,
-    ‘Judas Iscariotes’)
-    criarCartao(
-    ‘Bíblia’,
-    ‘ Qual o nome daquele que foi enviado antesde Jesus para preparar o caminho?’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
-    criarCartao(
-    ‘categoria’,
-    ‘pergunta’,
-    ‘resposta’)
+criaCartao(
+    'Matemática',
+    'Qual é a raiz quadrada de 121',
+    'É 11')
+    
+   criaCartao(
+    'Matemática',
+    'Qual é a fórmula de bhaskara?',
+    'É -b+-√b²-4ac/2a')
+    
+    criaCartao(
+    'historia',
+    'O que marcou o início da Guerra Fria?',
+    'o marco inicial do conflito foi um discurso feito por Harry Truman, presidente estadunidense, em 1947.')
+    
+    criaCartao(
+    'geografia',
+    'Quantos estados tem no Brasil',
+    'O Brasil é composto por 26 estados e um Distrito Federal.')
+    
+    criaCartao(
+    'Português',
+    'O que é um verbo?',
+    'O verbo é palavra que exprime ação, estado, mudança de estado, fenômeno da natureza. É a classe de palavras que apresenta mais flexões. Há flexões verbais de: número, pessoa, modo, tempo e voz.')
+    
+    criaCartao(
+    'Biologia',
+    'Quais as principais categorias taxonômicas',
+    'Reino, Filo, Classe, Ordem, Família, Gênero, Espécie.')
+    
+    criaCartao(
+    'Química',
+    'Quantos períodos possui na tabela periódica?',
+    'A tabela periódica tem sete períodos')
+    
+    criaCartao(
+    'matemática',
+    'Qual é a fórmula da área de um círculo?',
+    'A área de um círculo é pi vezes o raio elevado ao quadrado (A = π r²).')
+    
+    criaCartao(
+    'Quimica',
+    'Quais são os 3 estados físicos da matéria?',
+    'Sólido, líquido e gasoso.')
+    
+    criaCartao(
+    'Português',
+    'Quem foi o autor de "Dom Casmurro"?',
+    'Machado de Assis.')
+    
+    criaCartao(
+    'Quimica',
+    'Qual é a diferença entre uma reação endotérmica e uma reação exotérmica?',
+    'Reações endotérmicas absorvem energia (calor) do ambiente, enquanto reações exotérmicas liberam energia (calor) para o ambiente.')
+    
+    criaCartao(
+    'Biologia',
+    'Qual é a função do sistema digestivo do corpo humano?',
+    'O sistema digestivo é responsável por quebrar os alimentos em nutrientes que podem ser absorvidos e utilizados pelo corpo.')
+    
+    criaCartao(
+    'Historia',
+    'Quando iniciou e terminou a revolução industrial?',
+    'A revolução industrial iniciou em 1760 e terminou em 1840')
+    
+    criaCartao(
+    'Biologia',
+    'Qual é a função dos ribossomos nas células?',
+    'Produzir proteínas, através de um processo chamado de tradução.')
+    
+    criaCartao(
+    'História',
+    'Quando começou a segunda guerra mundial',
+    'Começou dia 1 de setembro de 1939')
+    
+    criaCartao(
+    'Geografia',
+    'Quantos continentes tem no mundo ?',
+    '7 continentes')
+    
+    criaCartao(
+    'Português',
+    'Quais são os pronomes pessoais restos?',
+    'Eu, tu, ele/ela, nós, vós, eles/elas')
+    
+    criaCartao(
+    'Biologia',
+    'Quais são os tipos sanguíneos existentes?',
+    'A, B, AB e O')
+    
+    criaCartao(
+    'Química',
+    'Quantas famílias tem na tabela periódica?',
+    'Tem 18 famílias')
+    
+    criaCartao(
+    'Matemática',
+    'Qual a fórmula da lei dos cossenos?',
+    ' c2=a2+b2–2abcos^c')
+    
     

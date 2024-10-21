956661500a9a7180a89579b7a052c39749301fd5
rebecaamaciel <142433287+rebecaamaciel@users.noreply.github.com>
2024-10-21 09:17:44 -0300

    fgbn

---
+++ pergunta.js
@@ -0,0 +1,81 @@
+criarCartao(
+    ‘Bíblia’,
+    ‘Quem usou um cajado para abrir o Mar Vermelho?’,
+    ‘Moisés’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Quem sobreviveu dentro de uma cova cheia de leões?’,
+    ‘Daniel’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Quem construiu uma arca para sobreviver ao dilúvio?’,
+    ‘Noé’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Qual foi o primeiro milagre que Jesus fez?’,
+    ‘transformar a água em vinho’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Quantos discípulos Jesus tinha?’,
+    ‘12’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Com quantos anos Jesus morreu?’,
+    ‘33’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Quantos livros tem a Bíblia?’,
+    ‘66’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Qual o nome do discípulo de Jesus que andou sobre as águas?’,
+    ‘Pedro’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘Qual discípulo traiu Jesus?’,
+    ‘Judas Iscariotes’)
+    criarCartao(
+    ‘Bíblia’,
+    ‘ Qual o nome daquele que foi enviado antesde Jesus para preparar o caminho?’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    criarCartao(
+    ‘categoria’,
+    ‘pergunta’,
+    ‘resposta’)
+    
